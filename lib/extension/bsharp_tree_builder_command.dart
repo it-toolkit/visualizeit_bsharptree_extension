1ad19d9e9b1a8d5ad1e4c895de4b68cca3dc1d54
Osiris <osirisarguello@gmail.com>
2024-05-15 19:36:58 -0300

    Arreglado warning en el widget y adaptacion a cambios en Model y Command

--- lib/extension/bsharp_tree_builder_command.dart
+++ lib/extension/bsharp_tree_builder_command.dart
@@ -1,12 +1,10 @@
 import 'package:visualizeit_bsharptree_extension/extension/bsharp_tree_model.dart';
 import 'package:visualizeit_extensions/common.dart';
 
-class BSharpTreeBuilderCommand extends ModelBuilderCommand{
-
+class BSharpTreeBuilderCommand extends ModelBuilderCommand {
   @override
-  BSharpTreeModel call() {
+  Model call(CommandContext context) {
     // TODO: implement call
     throw UnimplementedError();
   }
-  
 }
--- lib/extension/bsharp_tree_model.dart
+++ lib/extension/bsharp_tree_model.dart
@@ -1,13 +1,16 @@
 import 'package:visualizeit_extensions/common.dart';
 
-class BSharpTreeModel extends Model{
-  
-  
+class BSharpTreeModel extends Model {
   BSharpTreeModel(super.extensionId, super.name);
-  
+
   @override
   void apply(Command command) {
     // TODO: implement apply
   }
-  
+
+  @override
+  Model clone() {
+    // TODO: implement clone
+    throw UnimplementedError();
+  }
 }
