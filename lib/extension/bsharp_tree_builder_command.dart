42d47a10e3a18a9ad09fb0fe277c48124f75fe75
Fernando Mansilla <fernando.e.mansilla@gmail.com>
2024-07-03 23:50:23 -0300

    chore: Change creation command 'initialValues' arg type to intArray

--- lib/extension/bsharp_tree_builder_command.dart
+++ lib/extension/bsharp_tree_builder_command.dart
@@ -5,11 +5,12 @@ import 'package:visualizeit_extensions/scripting.dart';
 
 class BSharpTreeBuilderCommand extends ModelBuilderCommand {
   static final commandDefinition =
-      CommandDefinition(BSharpTreeExtension.extensionId, "bsharptree-create", [
+    CommandDefinition(BSharpTreeExtension.extensionId, "bsharptree-create", [
     CommandArgDef("maxCapacity", ArgType.int),
-    CommandArgDef("initialValues", ArgType.stringArray),
+    CommandArgDef("initialValues", ArgType.intArray),
     CommandArgDef("autoIncremental", ArgType.boolean, required: false, defaultValue: "false")
   ]);
+
   final int maxCapacity;
   final List<int> initialValues;
   final bool? autoIncremental;
@@ -17,18 +18,12 @@ class BSharpTreeBuilderCommand extends ModelBuilderCommand {
   BSharpTreeBuilderCommand(this.maxCapacity, this.initialValues,
       [this.autoIncremental]);
   BSharpTreeBuilderCommand.build(RawCommand rawCommand)
-      : maxCapacity =
-            commandDefinition.getArg(name: "maxCapacity", from: rawCommand),
-        initialValues = (commandDefinition.getArg(
-                name: "initialValues", from: rawCommand) as List<String>)
-            .map(int.parse)
-            .toList(),
-        autoIncremental =
-            commandDefinition.getArg(name: "autoIncremental", from: rawCommand);
+      : maxCapacity = commandDefinition.getArg(name: "maxCapacity", from: rawCommand),
+        initialValues = (commandDefinition.getArg(name: "initialValues", from: rawCommand) as List<int>),
+        autoIncremental = commandDefinition.getArg(name: "autoIncremental", from: rawCommand);
 
   @override
   BSharpTreeModel call(CommandContext context) {
-    return BSharpTreeModel(
-        "", maxCapacity, initialValues, autoIncremental ?? false); //TODO para que es el name?
+    return BSharpTreeModel("", maxCapacity, initialValues, autoIncremental ?? false); //TODO para que es el name?
   }
 }
